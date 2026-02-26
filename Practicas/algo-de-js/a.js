3926796ecc882210e823eb41df0c470717828cd0
Tak_0 from laptop <tmtroshko@gmail.com>
2026-02-26 10:01:48 +0100

    -Added JS tests

---
+++ Practicas/algo-de-js/a.js
@@ -0,0 +1,12 @@
+console.log("Hola mundo!");
+let nombre='nigga'
+let edad=20
+let curso='recoger algodon'
+
+console.log('Eso es un '+nombre+'. Y lo que hace es '+curso+'. Y lo hace por '+edad+' años.')
+let alfabeto="abcdefghijklmnopqrstuvwxyz";
+let frase = "casa";
+let paso = 3;
+for(let i = 0; i<alfabeto.length; i++){
+    
+}
