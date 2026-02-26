2c71bcab6a8bcf5bba31f09722ecc2c3cfac3a28
Tak_0 from laptop <tmtroshko@gmail.com>
2026-02-26 10:28:47 +0100

    fix

--- Practicas/algo-de-js/a.js
+++ Practicas/algo-de-js/a.js
@@ -1,12 +1,14 @@
-console.log("Hola mundo!");
-let nombre='nigga'
-let edad=20
-let curso='recoger algodon'
 
-console.log('Eso es un '+nombre+'. Y lo que hace es '+curso+'. Y lo hace por '+edad+' años.')
+let fraseCifrada = "";
+let letraCifrada = '';
 let alfabeto="abcdefghijklmnopqrstuvwxyz";
 let frase = "casa";
 let paso = 3;
-for(let i = 0; i<alfabeto.length; i++){
-    
+for(let i = 0; i<frase.length; i++){
+    let posicion=alfabeto.indexOf(frase[i])+paso % alfabeto.length
+    console.log(posicion)
+    letraCifrada=alfabeto[posicion]
+    fraseCifrada = fraseCifrada + letraCifrada; 
+
 }
+console.log(fraseCifrada)
