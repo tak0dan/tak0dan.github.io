477277308f86a9914a9e23e802bd0e314ea39733
installer <installer@gmail.com>
2026-03-05 10:22:45 +0100

    -refactor + JS folder

---
+++ Practicas/algo-de-js/js/app.js
@@ -0,0 +1,12 @@
+let num1=document.getElementById("numero1")
+let boton=document.getElementById("run")
+
+
+
+boton.addEventListener("click", (e) => {
+    console.log("Me han pulsado: " +num1.value)
+})
+
+
+console.log(num1.value)
+console.log(boton.id)
