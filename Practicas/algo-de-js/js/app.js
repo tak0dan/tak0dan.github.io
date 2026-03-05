dd81606b4713521fa21a14e033a52077e6f42176
installer <installer@gmail.com>
2026-03-05 11:59:27 +0100

    a

--- Practicas/algo-de-js/js/app.js
+++ Practicas/algo-de-js/js/app.js
@@ -1,12 +1,16 @@
 let num1=document.getElementById("numero1")
+let num2=document.getElementById("numero2")
 let boton=document.getElementById("run")
 
-
-
 boton.addEventListener("click", (e) => {
-    console.log("Me han pulsado: "+num1.value)
+    for(let i = 0; i<parseInt(num2); i++){
+     num1 = parseInt(num1)+parseInt(num1)
+    console.log(num1)
+}
 })
 
 
 console.log(num1.value)
 console.log(boton.id)
+
+
