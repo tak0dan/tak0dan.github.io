39070c8d104848e6d52b2e6447146be8f97fa9ae
installer <installer@gmail.com>
2026-03-06 08:56:03 +0100

    asd

--- Practicas/algo-de-js/js/app.js
+++ Practicas/algo-de-js/js/app.js
@@ -1,13 +1,20 @@
 let num1=document.getElementById("numero1")
 let num2=document.getElementById("numero2")
 let boton=document.getElementById("run")
+let salida=document.getElementById("salida")
+let borrar1=null
+let indice=0
 
 boton.addEventListener("click", (e) => {
-    for(let i = 0; i<parseInt(num2); i++){
-     num1 = parseInt(num1)+parseInt(num1)
-    console.log(num1)
+
+    indice++
+
+    let texto=""
+    texto+=`<input type="button" value="Borrar" id=${indice}>` + num1.value+"<br>"
+    salida.innerHTML=salida.innerHTML+texto
+    borrar1=document.getElementById("1")
 }
-})
+)
 
 
 console.log(num1.value)
